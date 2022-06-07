ab21fe8f7685ee177f55a0d225fd49da8f2f4d2b
Ravo1405 <ravonirina.rasolofomanana@yahoo.com>
2022-06-07 09:14:21 +0200

    Mise en forme en css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,83 @@
 
-
-fetch("./assets/json/Fnocs.json")
-.then((Fnocs)=>Fnocs.json())
-.then((data) => {
+fetch('./assets/json/Fnocs.json')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
     data = data.results;
     console.log(data);
 
+    let main = document.getElementById('all_Category');
 
     for (let i = 0; i < data.length; i++) {
         
-        let main = document.createElement('main');
-        let category= document.createElement('section');
-        let article = document.createElement('article');
-        let Src_Img = document.createElement('img');
-        let division = document.createElement('div');
-        let product_Name = document.createElement('h3');
-        let product_Description = document.createElement('p');
-        let price_Btn = document.createElement('button');
-        let add_Btn = document.createElement('button');
-
-
-        main.appendChild(section);
-        category.appendChild(article);
-        article.appendChild(Src_Img);
-        article.appendChild(division);
-        division.appendChild(product_Name);
-        division.appendChild(product_Description);
-        division.appendChild(price_Btn);
-        division.appendChild(add_Btn);
         
+        let category= document.createElement('section');
+        category.classList.add("category");
+        main.appendChild(category);
+
+        let produce = document.createElement('article');
+        produce.classList.add("produce");
+        category.appendChild(produce);
+
+        let Src_Img = document.createElement('img');
         Src_Img.src = data[i].Src_Img;
-        category.createTextNode = "(data[i].category)";
-        product_Description.createTextNode = "(data[i].product_Description)";
-        product_Name.createTextNode = "(data[i].product_Name)";
-        category.createTextNode = "(data[i].category)";
-      
+        Src_Img.classList.add("Src_Img");
+        produce.appendChild(Src_Img);
 
-       
+        let description = document.createElement('div');
+        description.classList.add("description");
+        produce.appendChild(description);
+        
+        let product_Name = document.createElement('h5');
+        let createTextNode_Name = document.createTextNode(data[i].product_Name);
+        product_Name.classList.add("product_Name");
+        description.appendChild(product_Name);
+        product_Name.appendChild(createTextNode_Name);
 
+        let product_Description = document.createElement('p');
+        let createTextNode_Description = document.createTextNode(data[i].product_Description);
+        product_Description.classList.add("product_Description");
+        description.appendChild(product_Description);
+        product_Description.appendChild(createTextNode_Description);
 
+        let price_Btn = document.createElement('p');
+        let createTextNode_Price = document.createTextNode(data[i].price_Btn);
+        price_Btn.classList.add("price_Btn");
+        description.appendChild(price_Btn);
+        price_Btn.appendChild(createTextNode_Price);
 
+        let add_Btn = document.createElement('button');
+        let createTextNode_Btn = document.createTextNode(data[i].add_Btn);
+        add_Btn.classList.add("add_Btn");
+        description.appendChild(add_Btn);
+        add_Btn.appendChild(createTextNode_Btn);
 
+        let identification = document.createElement('id');
+        // Element.setAttribute(name, value);
+        add_Btn.setAttribute("id",(data[i].id));
+        add_Btn.appendChild(identification);
+    
     }
 
 })
+        
+
+        
+        
+        
+
+        
+
+        
+
+        
+        
+        
+
+       
+
+        
 
 
 
